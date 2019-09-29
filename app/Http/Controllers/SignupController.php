7c8c46e87474f02f9145b10bf157d33c73aa15c7
Carlton Freeman <carltonfreeman@carltons-mbp.lan>
2019-09-29 14:57:59 -0400

    add missing dependency to signup controller

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
