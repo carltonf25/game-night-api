9e3a77eb892f0325ccfeb46fe804d978810f0dd5
carltonf25 <carltonfreeman24@gmailcom>
2019-10-07 22:48:13 -0400

    add auth contract to Need model

--- app/Models/Need.php
+++ app/Models/Need.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Auth\Authenticatable;
 use Laravel\Lumen\Auth\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
 use Illuminate\Database\Eloquent\Model;
 
