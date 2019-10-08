7a2a6e468d7658395e60da5862b3be524e17a592
carltonf25 <carltonfreeman24@gmailcom>
2019-10-07 23:04:20 -0400

    associate needs and events

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -33,7 +33,7 @@ class Event extends Model implements AuthenticatableContract, AuthorizableContra
 
   public function needs()
   {
-    return $this->hasMany('App\Models\Need');
+    return $this->belongsToMany('App\Models\Need');
   }
 
   public function user()
